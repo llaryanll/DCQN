92eb6b0769da56cad7b743c31549828e0edf391a
Aryan Mathur <66179175+llaryanll@users.noreply.github.com>
2024-01-21 11:03:51 +0530

    first

---
+++ pac_man.py
@@ -0,0 +1,213 @@
+
+
+# !pip install gymnasium     dependencies
+# !pip install "gymnasium[atari, accept-rom-license]"
+# !apt-get install -y swig
+# !pip install gymnasium[box2d]
+
+
+import os
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from collections import deque
+from torch.utils.data import DataLoader, TensorDataset
+
+
+
+class Network(nn.Module):
+  def __init__(self, action_size, seed=42):            #we did not take state_size since the input is not an array but at image
+    super(Network, self).__init__()
+    self.seed=torch.manual_seed(seed)
+
+    #CONVOLUTION
+
+    self.conv1 = nn.Conv2d(3, 32, kernel_size=8, stride=4) #first eye
+    self.bn1 = nn.BatchNorm2d(32)    #batch normalisation is done to improve performance
+    self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)  #all values are based on experimentation
+    self.bn2 = nn.BatchNorm2d(64)
+    self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+    self.bn3 = nn.BatchNorm2d(64)
+    self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+    self.bn4 = nn.BatchNorm2d(128)
+
+    #full connection
+
+    self.fc1 = nn.Linear(10*10*128, 512)   #10*10*128 is the o/p all convolutions
+    self.fc2 = nn.Linear(512, 256)
+    self.fc3 = nn.Linear(256, action_size)
+
+  def forward(self, state):
+    x = F.relu(self.bn1(self.conv1(state)));
+    x = F.relu(self.bn2(self.conv2(x)));
+    x = F.relu(self.bn3(self.conv3(x)));
+    x = F.relu(self.bn4(self.conv4(x)));
+
+    #flattening
+    x = x.view(x.size(0), -1)
+
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+
+    return self.fc3(x);
+
+
+import gymnasium as gym
+env = gym.make("MsPacmanDeterministic-v0", full_action_space=False)
+state_shape = env.observation_space.shape
+state_size  =  env.observation_space.shape[0];
+number_actions = env.action_space.n
+print("State shape ",state_shape)
+print("State size ",state_size)
+print("Number of actions ",number_actions)
+
+
+learning_rate = 5e-4 #based on experimentation
+minibatch_size = 64 #conventional
+discount_factor = 0.99
+
+
+#this is done to make our output from previous classes approopriate
+
+from PIL import Image
+from torchvision import transforms
+
+def preprocess_frame(frame):
+  #frame is in the format of a NumPy array
+  #converting numpy array to PIL format
+  frame = Image.fromarray(frame)
+
+  #tensorflow
+
+  preprocess = transforms.Compose([transforms.Resize((128,128)), transforms.ToTensor()]) #resizing the pacman grame from 210 size to 128 for ease in implementation
+  return preprocess(frame).unsqueeze(0) #batch dimension added (it is necessary in deep reinforcement learning)
+
+
+class Agent(): #this class defines the behaviour of the entire agent
+
+  def __init__(self, action_size):
+      self.device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+      self.action_size= action_size
+
+      #q-learning starts
+
+      self.local_qnetwork = Network(action_size).to(self.device) #this network will select the actions
+      self.target_qnetwork = Network(action_size).to(self.device) #this will calculate values that will be used in training the local model
+      self.optimizer =  optim.Adam(self.local_qnetwork.parameters(), lr=learning_rate) #the double network stabilizes the entire project
+
+      self.memory= deque(maxlen = 10000)
+
+  def step(self, state, action, reward, next_state, done): #stores all experiences and learns from them
+      state =  preprocess_frame(state)
+      next_state =  preprocess_frame(next_state)
+      self.memory.append((state, action, reward, next_state, done))
+      if len(self.memory) > minibatch_size:
+        experiences  = random.sample(self.memory, k=minibatch_size)
+        self.learn(experiences, discount_factor)
+
+  def act(self, state, epsilon=0.):  #this will help the agent choose the next action to take depending upon the current state of the network
+    state = preprocess_frame(state).to(self.device) #extra dimension for batch
+    self.local_qnetwork.eval() #evaluation mode
+    with torch.no_grad():
+      action_values = self.local_qnetwork(state) #forward propagation
+    self.local_qnetwork.train()
+    if random.random() > epsilon: #epsilon greedy policy is chosen for selecting the best Q-Value
+      return np.argmax(action_values.cpu().data.numpy()) #exploitation
+    else:
+      return random.choice(np.arange(self.action_size))
+
+  def learn(self,experiences, discount_factor):
+    states, actions, rewards,  next_states, dones =  zip(*experiences)
+
+    states = torch.from_numpy(np.vstack(states)).float().to(self.device)
+    actions = torch.from_numpy(np.vstack(actions)).long().to(self.device)
+    rewards = torch.from_numpy(np.vstack(rewards)).float().to(self.device)
+    next_states = torch.from_numpy(np.vstack(next_states)).float().to(self.device)
+    dones = torch.from_numpy(np.vstack(dones).astype(np.uint8)).float().to(self.device)
+
+    next_q_targets = self.target_qnetwork(next_states).detach().max(1)[0].unsqueeze(1)
+    q_targets = rewards + (discount_factor * next_q_targets * (1-dones))
+    q_expected =  self.local_qnetwork(states).gather(1, actions)
+    loss = F.mse_loss(q_expected,q_targets)
+    self.optimizer.zero_grad()
+
+    #back propagation (super easy for some reason)
+    loss.backward()
+    self.optimizer.step() #update model params
+
+
+agent = Agent(number_actions) #initialise
+
+
+number_episodes = 2000
+maximum_number_timesteps_per_episode = 10000
+epsilon_starting_value = 1.0
+epsilon_ending_value = 0.01
+epsilon_decay_value  = 0.995
+epsilon = epsilon_starting_value
+scores_on_100_episodes = deque(maxlen = 100) #basic dequq
+
+for episode in range(1, number_episodes + 1):
+  state, _ = env.reset()
+  score = 0
+  for t in range(maximum_number_timesteps_per_episode):
+    action = agent.act(state, epsilon) #do something
+    next_state, reward, done ,_, _ = env.step(action)
+    agent.step(state, action, reward, next_state, done) #training
+    state =  next_state
+    score += reward
+    if done:
+      break
+
+  scores_on_100_episodes.append(score)
+  epsilon = max(epsilon_ending_value, epsilon_decay_value*epsilon) #ensuring that epsilon doesnt reach its limit
+
+  print("\rEpisode {}\tAverage Score: {:.2f}".format(episode, np.mean(scores_on_100_episodes)), end = "")
+  if episode % 100 == 0:
+      print("\rEpisode {}\tAverage Score: {:.2f}".format(episode, np.mean(scores_on_100_episodes)))
+
+  if np.mean(scores_on_100_episodes) >= 500.0 : #experimental
+    print("\nEnvironmnet solved in {:d} episodes!\tAverage Score: {:.2f}".format(episode - 100, np.mean(scores_on_100_episodes)))
+    torch.save(agent.local_qnetwork.state_dict(), 'checkpoint.pth')
+    break
+
+
+import glob
+import io
+import base64
+import imageio
+from IPython.display import HTML, display
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+def show_video_of_model(agent, env_name):
+    env = gym.make(env_name, render_mode='rgb_array')
+    state, _ = env.reset()
+    done = False
+    frames = []
+    while not done:
+        frame = env.render()
+        frames.append(frame)
+        action = agent.act(state)
+        state, reward, done, _, _ = env.step(action)
+    env.close()
+    imageio.mimsave('video.mp4', frames, fps=30)
+
+show_video_of_model(agent, 'MsPacmanDeterministic-v0')
+
+def show_video():
+    mp4list = glob.glob('*.mp4')
+    if len(mp4list) > 0:
+        mp4 = mp4list[0]
+        video = io.open(mp4, 'r+b').read()
+        encoded = base64.b64encode(video)
+        display(HTML(data='''<video alt="test" autoplay
+                loop controls style="height: 400px;">
+                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+             </video>'''.format(encoded.decode('ascii'))))
+    else:
+        print("Could not find video")
+
+show_video()
